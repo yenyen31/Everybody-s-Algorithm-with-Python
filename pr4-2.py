71ad86cd03acf30544048198dd33df20de222cc3
Yen31 <82165728+yenyen31@users.noreply.github.com>
2021-10-12 22:12:53 +0900

    Update pr4-2.py

--- pr4-2.py
+++ pr4-2.py
@@ -2,15 +2,15 @@
 
 def find_max(i, n):
     if n == 1:
-        return a[0]
+        return i[0]
 
-    max_val = find_max(a, n-1)
+    max_val = find_max(i, n-1)
 
-    if max_val > a[n-1]:
+    if max_val > i[n-1]:
         return max_val
 
     else:
-        return a[n-1]
+        return i[n-1]
 
     v = [0, 1, 2, 3, 4, 5]
     print(find_max(v, len(v)))
