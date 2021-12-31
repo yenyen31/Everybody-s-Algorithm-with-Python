112e123134bcafd293928d9e9daeb041f51a4bf9
Yen31 <82165728+yenyen31@users.noreply.github.com>
2021-12-31 16:46:48 +0900

    Delete python workspace directory

--- python workspace/pr1-1.py
+++
@@ -1,8 +0,0 @@
-def one(n):
-    m = 0
-    for i in range(1, n + 1):
-        m += i * i
-
-    return m
-
-    print(one(10))
--- python workspace/pr3-1.py
+++
@@ -1,9 +0,0 @@
-def bf(a):
-    n = len(a)
-    for i in range(0, n - 1):
-        for j in range(i + 1, n):
-            print(a[i], " - ", a[j])
-
-
-name = ["Tom", "Jerry", "Mike"]
-bf(name)
